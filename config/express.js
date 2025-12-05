9dcdfc0a670a61c5cf3c61fa7fa07fdd8a53248f
luisa-ralima <163539610+luisa-ralima@users.noreply.github.com>
2025-12-05 14:20:22 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -8,22 +8,28 @@ var path = require('path');
 module.exports = function () {
   var app = express();
 
+  // View engine
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, '../app/views'));
 
+  // Arquivos estáticos
   app.use(express.static(path.join(__dirname, '../app/public')));
 
+  // Body Parser
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(express.json());
 
+  // Validator
   app.use(expressValidator());
 
+  // Session usando variável de ambiente
   app.use(session({
-    secret: 'seuSegredoAqui',
+    secret: process.env.SESSION_SECRET, // ← vem do .env carregado no app.js
     resave: false,
     saveUninitialized: false
   }));
 
+  // Carregar rotas e infraestrutura
   load('routes', { cwd: 'app' })
     .then('infra')
     .into(app);
