9e7b84205298559779721cf4c6288bb8e8bbdedb
luisa-ralima <163539610+luisa-ralima@users.noreply.github.com>
2025-12-05 18:43:21 -0300

    Update connectionFactory.js

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,17 +1,13 @@
 var mysql = require('mysql');
 
-var connectMYSQL = function () {
-    return mysql.createConnection({
-        host: process.env.MYSQLHOST,
-        user: process.env.MYSQLUSER,
-        password: process.env.MYSQLPASSWORD,
-        database: process.env.MYSQLDATABASE,
-        port: process.env.MYSQLPORT
-    });
+module.exports = function() {
+  return function() {
+      return mysql.createConnection({
+          host: process.env.MYSQLHOST,
+          user: process.env.MYSQLUSER,
+          password: process.env.MYSQLPASSWORD,
+          database: process.env.MYSQLDATABASE,
+          port: process.env.MYSQLPORT
+      });
+  };
 }
-
-module.exports = function () {
-    return connectMYSQL;
-}
-
-
